d4708307459cc97d680df1b8724dcc5c63789816
garradefogo124 <micaelmiranda124@gmail.com>
2022-10-23 22:02:16 -0300

    Jogo Mario Jump completo

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const game = document.querySelector('main')
+const distance = document.querySelector('#distance')
+const character = document.querySelector('.character')
+const obstacle = document.querySelector('.obstacle')
+const enemy = document.querySelector('.enemy')
+const clouds = document.querySelector('.clouds')
+const gameOver = document.querySelector('.game-over')
+const tryAgain = document.querySelector('#try-again')
+const highScore = document.querySelector('#high')
+let end = false
+let travelledDistance = 0000
+
+const setJump = () => {
+    character.classList.add('jump')
+
+    setTimeout(() => {
+        character.classList.remove('jump')
+    }, 700)
+}
+
+const loop = setInterval(() => {
+    const obstaclePosition = obstacle.offsetLeft
+    const enemyPosition = enemy.offsetLeft
+    const cloudsPosition = clouds.offsetLeft
+    const characterPosition = +window.getComputedStyle(character).bottom.replace('px', '')
+
+    if ((obstaclePosition <= 120 && characterPosition < 80 && obstaclePosition > 0) ||
+        (enemyPosition <= 120 && enemyPosition > 0 && characterPosition < 60)) {
+
+        obstacle.style.animation = 'none'
+        obstacle.style.left = `${obstaclePosition}px`
+
+        enemy.style.animation = 'none'
+        enemy.style.left = `${enemyPosition}px`
+
+        clouds.style.animation = 'none'
+        clouds.style.left = `${cloudsPosition}px`
+
+        character.style.animation = 'none'
+        character.style.bottom = `${characterPosition}px`
+
+        character.src = './img/game-over.png'
+        character.style.width = '80px'
+        character.style.marginLeft = '50px'
+
+        gameOver.style.display = 'block'
+        game.style.filter = 'grayscale(50%)'
+
+        if (localStorage.getItem('highScore') < travelledDistance) {
+            localStorage.setItem('highScore', travelledDistance)
+        }
+        highScore.textContent = localStorage.getItem('highScore')
+
+
+        end = true
+        clearInterval(loop)
+    }
+}, 10)
+
+const distanceCounter = setInterval(() => {
+    if (end === false) {
+        travelledDistance++
+        distance.textContent = travelledDistance
+    }
+}, 200)
+
+document.addEventListener('keydown', setJump)
+game.addEventListener('click', setJump)
+
+tryAgain.addEventListener('click', function () {
+    window.location.reload()
+})
